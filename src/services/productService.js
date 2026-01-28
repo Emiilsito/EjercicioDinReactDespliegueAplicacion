a3cc3906daa558451b405104010c0f38bea301ac
emigarmun <emiliogg200@gmail.com>
2026-01-28 16:34:41 +0100

    Cambios

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,4 +1,4 @@
-const API_URL = `${import.meta.env.VITE_API_URL}`;
+const API_URL = `${import.meta.env.VITE_API_URL}`; //Comment
 
 export const mapProductoFromAPI = (producto) => ({
     id: producto._id,
